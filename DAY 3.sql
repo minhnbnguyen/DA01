a43ed807aff016471328e887b941b077b631c039
minhnbnguyen <142035875+minhnbnguyen@users.noreply.github.com>
2023-10-09 17:10:26 -0500

    Create DAY 3.sql

---
+++ DAY 3.sql
@@ -0,0 +1,15 @@
+--- Japanese Cities Attributes on Hacker Rank
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+--- Revising the select query on HackerRank
+SELECT NAME
+FROM CITY
+WHERE population > 120000
+AND COUNTRYCODE = 'USA';
+--- Weathere Observation Station 1 HackerRank
+SELECT CITY, STATE
+FROM STATION;
+--- Weather Observation Station 6 HackerRank
+Chua lam dc 
+--- Weather Observation Station 7
