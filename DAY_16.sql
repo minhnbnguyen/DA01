a40af7e6586ac2f5a9e31d1381bafa3f42d2e1fa
minhnbnguyen <142035875+minhnbnguyen@users.noreply.github.com>
2024-02-03 14:35:31 -0600

    Update DAY_16.sql

--- DAY_16.sql
+++ DAY_16.sql
@@ -1,5 +1,4 @@
 -- ex1: leetcode-mmediate-food-delivery.
-# Write your MySQL query statement below
 SELECT
     ROUND (avg(order_date=customer_pref_delivery_date)*100,2) AS immediate_percentage
 FROM Delivery
@@ -10,6 +9,24 @@ WHERE (customer_id, order_date) IN
     GROUP BY customer_id
 )
 -- ex2: leetcode-game-play-analysis.
+WITH c AS
+(
+SELECT *
+FROM
+(
+SELECT *,
+DATEDIFF (prev_event_date, event_date) AS time_period
+FROM
+(
+SELECT *,
+LEAD (event_date,1,"2024-03-01") OVER (PARTITION BY player_id ORDER BY event_date) AS prev_event_date
+FROM Activity
+) AS a
+) AS b
+GROUP BY player_id
+)
+SELECT ROUND (avg(time_period=1),2) AS fraction
+FROM c
 -- ex3: leetcode-exchange-seats.
 -- ex4: leetcode-restaurant-growth.
 -- ex5: leetcode-investments-in-2016.
