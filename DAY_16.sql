53ce62e25ad8c263b45d60ff9fd4a7b804ab3fab
minhnbnguyen <142035875+minhnbnguyen@users.noreply.github.com>
2024-02-02 21:15:54 -0600

    Update DAY_16.sql

--- DAY_16.sql
+++ DAY_16.sql
@@ -1,31 +1,14 @@
 -- ex1: leetcode-mmediate-food-delivery.
 # Write your MySQL query statement below
-WITH stt AS
-(
 SELECT
-COUNT(*) AS immediate
-FROM (
-SELECT delivery_id,
-CASE
-    WHEN order_date = customer_pref_delivery_date THEN 'immediate'
-    ELSE 'scheduled'
-END AS status
-FROM Delivery) AS a
-WHERE status = 'immediate'
-),
-order_num AS
-(
-SELECT COUNT(*) AS first_order
-FROM
-(
-SELECT delivery_id, customer_id, order_date,
-DENSE_RANK () OVER (PARTITION BY customer_id ORDER BY order_date) AS order_number
+    ROUND (avg(order_date=customer_pref_delivery_date)*100,2) AS immediate_percentage
 FROM Delivery
-) AS c
-WHERE order_number = 1    
+WHERE (customer_id, order_date) IN
+(
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
 )
-SELECT stt.immediate/order_num.first_order AS immediate_percentage
-FROM stt JOIN order_num ON stt.delivery_id = c.delivery_id
 -- ex2: leetcode-game-play-analysis.
 -- ex3: leetcode-exchange-seats.
 -- ex4: leetcode-restaurant-growth.
